9bcff07fae8b780b39a759c44e8df1fb0a7a67b3
PorSiang <porsiang.tiew@vvinners.com>
2025-11-30 15:44:24 +0800

    debug5

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/scss/index.scss',
-                'resources/scss/home.scss',
-                'resources/scss/about_us.scss',
-                'resources/scss/contact.scss',
             ],
             buildDirectory: 'dist',
             refresh: true,
